85042f3f7928e90ce5b8a19e6974e891cc62d45c
Eric Viana <81055306+EndriXzzz@users.noreply.github.com>
2021-12-20 17:20:33 -0300

    Add files via upload

---
+++ src/jogoaps/Modelbean.java
@@ -0,0 +1,34 @@
+package jogoaps;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author win
+ */
+public class Modelbean {
+    
+    String nome;
+    int pontos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+    
+}
